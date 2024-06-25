c253bf99bd057d8b85250b45d491b0522e4c4294
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-06-25 15:36:26 +0200

    feat: TechnologiesTableSeeder completed

---
+++ database/seeders/TechnologiesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+class TechnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $technologies = ['html', 'css', 'javascript', 'vue.js', 'vite', 'node.js' ,'sass', 'bootstrap', 'php', 'mysql', 'blade', 'laravel'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology);
+            $newTechnology->color = $faker->colorName();
+            $newTechnology->save();
+        }
+    }
+}
